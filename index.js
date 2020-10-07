1971f022f8c4df8e9ec4636dd531959d2895ca38
Victor Iris <victormidp@gmail.com>
2020-10-07 14:51:03 -0500

    Reconfigure api for custom setup

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const CubejsServer = require('@cubejs-backend/server');
+require("dotenv").config();
+const http = require("http");
+const bodyParser = require("body-parser");
+const CubejsServerCore = require("@cubejs-backend/server-core");
+const WebSocketServer = require("@cubejs-backend/server/WebSocketServer");
+const express = require("express");
 
-const server = new CubejsServer();
+const app = express();
+app.use(require("cors")());
+app.use(bodyParser.json({ limit: "50mb" }));
 
-server.listen().then(({ version, port }) => {
-  console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
-}).catch(e => {
-  console.error('Fatal error during server start: ');
-  console.error(e.stack || e);
+const serverCore = CubejsServerCore.create({
+	logger: (msg, params) => {
+		console.log(`${msg}: ${JSON.stringify(params)}`);
+  },
+	checkAuthMiddleware: (req, res, next) => {
+    return next && next();
+  },
+  checkAuth: (req, auth) => {} 
+});
+
+serverCore.initApp(app);
+const server = http.createServer({}, app);
+
+// Subscriptions thru websockets
+const socketServer = new WebSocketServer(serverCore, {
+  processSubscriptionsInterval: 1,
+});
+socketServer.initServer(server);
+
+const port = process.env.PORT || 4000;
+server.listen(port, (err) => {
+	if (err) {
+		console.error("Fatal error during server start: ");
+		console.error(e.stack || e);
+	}
+	console.log(
+		`Cube.js server (${CubejsServerCore.version()}) is listening on ${port}`
+	);
 });
